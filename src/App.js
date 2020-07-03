128ca67d578b63e365ccc63035c86ec541abeccc
procopio420 <lucas-procopio@outlook.com>
2020-07-03 04:08:25 -0300

    cc

--- src/App.js
+++ src/App.js
@@ -9,33 +9,33 @@ import VideoPage from './components/content/VideoPage/VideoPage';
 import SearchResult from './components/content/SearchResult';
 import NotFound from './components/content/NotFound';
 import InitialPage from './components/content/InitialPage';
-import VideoListProvider from './contexts/VideoListProvider'
+import VideoListProvider from './contexts/VideoListProvider';
 
 require('dotenv').config();
 
 const App = () => (
   <Router>
     <VideoListProvider>
-    <div className="App">
-      <Header />
-      <Switch>
-          <Route exact path="/">
-            <InitialPage />
+      <div className="App">
+        <Header />
+        <Switch>
+            <Route exact path="/">
+              <InitialPage />
+            </Route>
+            <Route
+              exact
+              path="/watch/:videoId" render={(props) => <VideoPage {...props} />}
+            />
+            <Route
+              exact
+              path="/results/:searchParam" render={(props) => <SearchResult {...props} />}
+            />
+          <Route path="*">
+            <NotFound />
           </Route>
-          <Route
-            exact
-            path="/watch/:videoId" render={(props) => <VideoPage {...props} />}
-          />
-          <Route
-            exact
-            path="/results/:searchParam" render={(props) => <SearchResult {...props} />}
-          />
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </div>
-  </VideoListProvider>
+        </Switch>
+      </div>
+    </VideoListProvider>
   </Router>
  
 );
--- src/contexts/VideoListProvider.js
+++ src/contexts/VideoListProvider.js
@@ -5,9 +5,7 @@ export const VideoListContext = createContext(null);
 export default ({ children }) => {
   const [videoList, setVideoList] = React.useState([]);
 
-  const store = {
-    videoList: [videoList, setVideoList],
-  };
+  const store = { videoList: [videoList, setVideoList] };
 
   return <VideoListContext.Provider value={store}>{children}</VideoListContext.Provider>;
 };
