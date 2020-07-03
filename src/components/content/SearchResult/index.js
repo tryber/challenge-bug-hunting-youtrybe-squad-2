fb38e6a95fbc5191009172771001c9207d9225c8
procopio420 <lucas-procopio@outlook.com>
2020-07-03 02:27:16 -0300

    agr vai

--- src/components/content/SearchResult/index.js
+++ src/components/content/SearchResult/index.js
@@ -5,6 +5,22 @@ import VideoCard from './VideoCard/VideoCard';
 import '../../../css/sideBar.css';
 import { searchVideos } from '../../../api/service';
 
+const Video = (props) => {
+  const { item, data } = props;
+  return (
+    <Link
+      className="thumbnail-card"
+      key={item.etag}
+      to={{
+        pathname: `/watch/${item.id.videoId}`,
+        state: { data },
+      }}
+    >
+      <VideoCard video={item} />
+    </Link>
+  )
+}
+
 const SearchResult = (props) => {
   const { params: { searchParam } } = props.match;
 
@@ -29,16 +45,7 @@ const SearchResult = (props) => {
   return (
     <div>
       {data.map((item) => (
-        <Link
-          className="thumbnail-card"
-          key={item.etag}
-          to={{
-            pathname: `/watch/${item.id.videoId}`,
-            state: { data },
-          }}
-        >
-          <VideoCard video={item} />
-        </Link>
+        <Video item={item} data={data}/>
       ))}
     </div>
   );
